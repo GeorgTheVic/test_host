032bbddd05cd18a24dd3d60e5394eba9dd6c9e83
GeorgTheVic <georgthevic@gmail.com>
2023-03-16 17:03:31 +0600

    thats was massive changes

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return render_template('/frontend/app/index.html')
-
+def index():
+    return render_template('app/index.html')
 
 if __name__ == '__main__':
     app.run()
