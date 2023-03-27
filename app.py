526d5351c1528bd6b84e5bfe99ac1a7861c06223
Damir <damir.urmanov2003@mail.ru>
2023-03-27 21:25:52 +0600

    added template tags, initialized db, added admin

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template
+import os
+from flask_babelex import Babel
+from flask import Flask, render_template, request, session
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from models import Info, db
 
 app = Flask(__name__)
+babel = Babel(app)
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.getcwd() + "site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+db.init_app(app)
 
 
-@app.route('/')
+# @babel.localeselector
+# def get_locale():
+#     if request.args.get("lang"):
+#         session["lang"] = request.args.get("lang")
+#     return session.get("lang", "en")
+
+
+# only once
+with app.app_context():
+    db.create_all()
+
+app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
+
+admin = Admin(app, name="gym_investor", template_mode="bootstrap3")
+admin.add_view(ModelView(Info, db.session))
+
+
+@app.route("/")
 def index():
-    return render_template('app/index.html')
+    return render_template("app/index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
